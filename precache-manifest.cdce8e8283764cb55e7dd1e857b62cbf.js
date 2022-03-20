9a60d1d87c87006676be8215c766b925600aff7a
dot-cmyk <gdotcmyk@gmail.com>
2022-03-20 22:52:06 +0900

    Updates

---
+++ precache-manifest.cdce8e8283764cb55e7dd1e857b62cbf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "67c7507df279b6cb578c",
+    "url": "/croquistool/static/js/main.b5f98eb8.chunk.js"
+  },
+  {
+    "revision": "1f07580171c0804b38fa",
+    "url": "/croquistool/static/js/runtime~main.3dc749dc.js"
+  },
+  {
+    "revision": "3a2ac3716427a8b80029",
+    "url": "/croquistool/static/js/2.e11b6d59.chunk.js"
+  },
+  {
+    "revision": "ba98c55948d4629202b755cd81169acb",
+    "url": "/croquistool/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/croquistool/precache-manifest.cdce8e8283764cb55e7dd1e857b62cbf.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/croquistool/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
