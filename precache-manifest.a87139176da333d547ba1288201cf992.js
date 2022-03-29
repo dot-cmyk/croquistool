c78f701b4b241eb083fb1a92659f85a83943212d
dot-cmyk <gdotcmyk@gmail.com>
2022-03-29 20:01:11 +0900

    Updates

---
+++ precache-manifest.a87139176da333d547ba1288201cf992.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87a62522a4ef696c209a",
+    "url": "/croquistool/static/css/main.67eee51d.chunk.css"
+  },
+  {
+    "revision": "87a62522a4ef696c209a",
+    "url": "/croquistool/static/js/main.85e34061.chunk.js"
+  },
+  {
+    "revision": "1f07580171c0804b38fa",
+    "url": "/croquistool/static/js/runtime~main.3dc749dc.js"
+  },
+  {
+    "revision": "d7bd78953e47cf405ffb",
+    "url": "/croquistool/static/js/2.10dde754.chunk.js"
+  },
+  {
+    "revision": "c34c720d5f84d84468a88be6abfb8f2f",
+    "url": "/croquistool/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/croquistool/precache-manifest.f03191b6ef835a93e0411ef311620a3c.js"
+  "/croquistool/precache-manifest.a87139176da333d547ba1288201cf992.js"
 );
 
 workbox.clientsClaim();
