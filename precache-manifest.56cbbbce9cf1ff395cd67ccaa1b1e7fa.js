06e59d5d187e8feba501569237c810e48f5fb41f
dot-cmyk <gdotcmyk@gmail.com>
2022-04-18 00:03:34 +0900

    Updates

---
+++ precache-manifest.56cbbbce9cf1ff395cd67ccaa1b1e7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d3cf41816ea10980fc2",
+    "url": "/croquistool/static/css/main.edf69feb.chunk.css"
+  },
+  {
+    "revision": "1d3cf41816ea10980fc2",
+    "url": "/croquistool/static/js/main.bfd71a31.chunk.js"
+  },
+  {
+    "revision": "1f07580171c0804b38fa",
+    "url": "/croquistool/static/js/runtime~main.3dc749dc.js"
+  },
+  {
+    "revision": "adb01231e93f5e09d59b",
+    "url": "/croquistool/static/css/2.4de601d7.chunk.css"
+  },
+  {
+    "revision": "adb01231e93f5e09d59b",
+    "url": "/croquistool/static/js/2.1acd9303.chunk.js"
+  },
+  {
+    "revision": "5be668f002e2fdb1cd735528183009c2",
+    "url": "/croquistool/index.html"
+  }
+];
