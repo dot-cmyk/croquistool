85a13d09e19b4614a297843c2d3801315ca92aab
dot-cmyk <gdotcmyk@gmail.com>
2022-11-14 23:37:32 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/croquistool/precache-manifest.16f9904fec7df488f40197391f61b736.js"
+  "/croquistool/precache-manifest.3e2590a2449df05a1aa72ef970aaab71.js"
 );
 
 workbox.clientsClaim();
